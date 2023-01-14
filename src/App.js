79a20525b3a3597fb26c0a14d2ce17dcb6824557
Holland Pleskac <hollandpleskac@gmail.com>
2023-01-14 13:52:33 -0800

    login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Main from './pages/main';
-
+import Login from './pages/login';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Main/>}/>
+      <Route path='/login' element={<Login/>}/>
     </Routes>
   );
 }
