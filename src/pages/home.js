7bdaad1dbe0a9e6f566499aa2c995616eb2f061b
alskaf1293 <timothyohoc@gmail.com>
2023-01-15 05:36:40 -0800

    added more styles

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,8 +3,6 @@ import Template from '../components/Template'
 import { useNavigate } from 'react-router-dom'
 import RedirectWrapper from '../components/redirect';
 
-
-
 const Home = (props) => {
   const navigate = useNavigate();
 
@@ -18,14 +16,19 @@ const Home = (props) => {
     navigate('/passenger-destination')
   }
 
+  const backgroundStyle={
+    //https://images.unsplash.com/photo-1622547748225-3fc4abd2cca0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80
+    backgroundImage: "url('https://images.unsplash.com/photo-1622547748225-3fc4abd2cca0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80')"
+  }
+
   return (
-      <Template title='Home' >
-        <div className='flex flex-col h-full w-full justify-center items-center gap-y-8' >
-          <div onClick={onClickDriver} className='w-[300px] h-[200px] bg-blue-500 text-white flex justify-center items-center rounded cursor-pointer'>
-            Driver
+      <Template title='CarSurf' >
+        <div style={backgroundStyle} className='flex flex-col h-full w-full justify-center items-center gap-y-8' >
+          <div onClick={onClickDriver} className='w-[300px] h-[100px] font-sans bg-slate-800 text-white flex justify-center items-center rounded cursor-pointer'>
+            <text className="font-sans text-xl">Driver</text>
           </div>
-          <div onClick={onClickPassenger} className='w-[300px] h-[200px] bg-blue-500 text-white flex justify-center items-center rounded cursor-pointer' >
-            Passenger
+          <div onClick={onClickPassenger} className='w-[300px] h-[100px] font-sans bg-slate-800 text-white flex justify-center items-center rounded cursor-pointer' >
+            <text className="font-sans text-xl">Passenger</text>
           </div>
         </div>
       </Template>
