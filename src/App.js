539436f606d4aac1feb577ae2285cca1cf8ff7be
Holland Pleskac <hollandpleskac@gmail.com>
2023-01-14 16:04:33 -0800

    authentication

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Main from './pages/main';
 import PassengerRides from './pages/passenger-rides';
 import { Route, Routes } from 'react-router-dom';
 import DriverRequests from './pages/driver-requests';
+import Login from './pages/login';
+import Home from './pages/home';
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
       <Route path='/' element={<Main />} />
       <Route path='/passenger-rides' element={<PassengerRides />} />
       <Route path='driver-requests' element={<DriverRequests />} />
+      <Route path='login' element={<Login />} />
+      <Route path='home' element={<Home />} />
+
     </Routes>
   );
 }
--- src/components/Template.js
+++ src/components/Template.js
@@ -3,16 +3,25 @@ import Signout from '../assets/Signout'
 import Logo from '../assets/Logo'
 
 const Template = (props) => {
+
+  const onClickLogo = () => {
+    console.log("click home")
+  }
+
+  const onClickSignout = () => {
+    console.log('click signout')
+  }
+
   return (
     <div className='w-full h-screen' >
       <div className='p-6 bg-blue-500 flex items-center shadow' >
         <div className='flex items-center mr-auto' >
-          <div className='flex justify-center items-center w-[40px] h-[40px]' >
+          <div onClick={onClickLogo} className='flex justify-center items-center w-[40px] h-[40px]' >
             <Logo />
           </div>
           <h1 className='ml-4 text-2xl font-semibold text-white' >{props.title}</h1>
         </div>
-        <div className='flex items-center justify-center w-[40px] h-[40px]' >
+        <div onClick={onClickSignout} className='flex items-center justify-center w-[40px] h-[40px]' >
           <Signout />
         </div>
       </div>
